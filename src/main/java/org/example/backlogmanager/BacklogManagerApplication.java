baa1a38eeee72ee7275aa94a322eaef844f65ba0
Nimo <nassimouali484@gmail.com>
2025-11-27 20:48:08 +0100

    init

---
+++ src/main/java/org/example/backlogmanager/BacklogManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.backlogmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacklogManagerApplication.class, args);
+    }
+
+}
